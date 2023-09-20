19993148f0039c20fc527c74cf0feffd93e99f90
Arcenox <albert@arcenox.com>
2023-09-20 17:26:50 +0200

    removed the broker folder

--- src/@COMPANYNAME@.@PROJECTNAME@.@SERVICE@.MessageBroker/ChannelConfig/Client/IClient.cs
+++
@@ -1,9 +0,0 @@
-﻿using Grpc.Net.Client;
-
-namespace _COMPANYNAME_._PROJECTNAME_._SERVICE_.MessageBroker.ChannelConfig.Client
-{
-    public interface IClient
-    {
-        GrpcChannel CoreChannel { get; }
-    }
-}
